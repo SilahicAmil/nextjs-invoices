bc5804e9505b79f4f66dcf848cd4e527dc9877fa
Amil Silahic <jinobie02@gmail.com>
2022-10-18 12:07:15 -0600

    added base func to NewInvoice and TODO coments

--- components/NewInvoice/NewInvoice.js
+++ components/NewInvoice/NewInvoice.js
@@ -1,14 +1,56 @@
+import { useRef } from "react";
+
 const NewInvoice = (props) => {
+  const firstNameRef = useRef();
+  const lastNameRef = useRef();
+  const dateRef = useRef();
+  const amountRef = useRef();
+
+  const submitFormHandler = (e) => {
+    e.preventDefault();
+    console.log("submited");
+    // extract this to a hook later
+
+    const firstNameRefValue = firstNameRef.current.value;
+    const lastNameRefValue = lastNameRef.current.value;
+    const dateRefValue = dateRef.current.value;
+    const amountRefValue = amountRef.current.value;
+
+    // Redux would be perfect for this
+    const inputData = {
+      firstName: firstNameRefValue,
+      lastName: lastNameRefValue,
+      dueDate: new Date(dateRefValue).toLocaleDateString("en-US"),
+      amount: amountRefValue,
+    };
+
+    console.log(inputData);
+    e.target.reset();
+  };
+
   return (
-    // find maybe something to install that makes customizable fors
-    // mainly because of the date
+    // find maybe something to install that makes customizable forms
+    // mainly because
     // styling would also be a pain
     // maybe try it by hand idk YOLO
 
-    <div>
-      <form action="">
-        <label htmlFor=""></label>
-        <input type="text" />
+    <div className="p-6 bg-[#1E213B] rounded-3xl w-screen  justify-center items-center">
+      <form
+        action=""
+        className="grid   items-center  justify-center  gap-2"
+        onSubmit={submitFormHandler}
+      >
+        <label htmlFor="">First Name</label>
+        <input type="text" ref={firstNameRef} />
+        <label htmlFor="">Last Name</label>
+        <input type="text" ref={lastNameRef} />
+        <label htmlFor="">Due Date</label>
+        <input type="date" ref={dateRef} />
+        <label htmlFor="">Amount</label>
+        <input type="number" min="100" max="100000" step="1" ref={amountRef} />
+        <div>
+          <button className="p-4 bg-[#3D9588] mt-5 rounded-lg">Submit</button>
+        </div>
       </form>
     </div>
   );
